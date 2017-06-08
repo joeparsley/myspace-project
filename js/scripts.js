cc34bfc3823422861d7bc8dc0a58aa0b9761ecd1
Jared Eiseman and Joe Parsley <jared@>
2017-06-08 11:38:37 -0700

    finish best-in section

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,27 @@ $(document).ready(function() {
 
   $(window).on('resize', function() {
     resize();
+    $('.card-image img').each(function(index) {
+      $(this).attr('src', 'img/cards/600x600(' + index + ').jpg');
+      $(this).height($(this).width() / 2);
+    });
   });
 
+  $('.card-image img').each(function(index) {
+    $(this).attr('src', 'img/cards/600x600(' + index + ').jpg');
+    $(this).height($(this).width() / 2);
+  });
+
+  var expanded = false;
+  var navHeight = $('.content-nav').height();
+  $('.content-nav a').click(function() {
+    if (!expanded) {
+      $('.content-nav').animate({height: '1000px'}, 1000);
+      expanded = true;
+    } else {
+      $('.content-nav').animate({height: navHeight}, 1000);
+    }
+  })
   // var i = 2;
   // setInterval(function() {
   //   $('.main-square img').attr('src', 'img/hero/hero' + i +'.jpg');
